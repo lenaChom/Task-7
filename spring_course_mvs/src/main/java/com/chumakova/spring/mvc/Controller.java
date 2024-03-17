28f10b45613037f433de42d51c9329bd2761c2c2
Елена <149941943+lenaChom@users.noreply.github.com>
2024-03-17 12:31:42 +0300

    Delete spring_course_mvs directory

--- spring_course_mvs/src/main/java/com/chumakova/spring/mvc/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.chumakova.spring.mvc;
-
-@org.springframework.stereotype.Controller
-public class Controller {
-    public String showFirstView(){
-        @ReqestMapping
-        return "first-view";
-    }
-}
